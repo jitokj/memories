076b628a360a8bff5678d25e3ed2afb067516c0a
jito <jitokj3@gmail.com>
2020-12-28 22:05:39 +0530

    added folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
-
+import React from "react";
+import { Container, Typography, Grow, Grid, AppBar } from "@material-ui/core";
+import memories from "./images/memories.jpg";
+import Posts from "./components/posts/Posts";
+import Form from "./components/form/Form";
+import useStyles from "./styles";
 const App = () => {
-    return (
-        <div>
-         <h1>App</h1>   
-        </div>
-    );
+  const classes = useStyles();
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
